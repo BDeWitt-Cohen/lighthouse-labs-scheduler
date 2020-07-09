6030f6f80cae696c10786b3b914b1f22243cf461
Trevor Thomas <thomastrevor1@gmail.com>
2020-07-09 09:29:47 -0600

    Added interviewer formatting function

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index.js"
-import getAppointmentsForDay from "helpers/selectors"
+import {getAppointmentsForDay, getInterview} from "helpers/selectors"
 
 
 
@@ -19,17 +19,20 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   useEffect(() => {
     
     Promise.all([
       axios.get(`/api/days`),
-      axios.get(`/api/appointments`)
+      axios.get(`/api/appointments`),
+      axios.get(`/api/interviewers`)
     ]).then((all) => {
       console.log(all[0]); // first
       console.log(all[1]); // second
+      console.log(all[2].data);
       setState(prev => ({...state, days: all[0].data,...appointments, appointments: all[1].data}));
         
     })
@@ -37,7 +40,24 @@ export default function Application(props) {
         console.log(err.message);
         })
 }, []);
- const appointments = getAppointmentsForDay(state, state.day)
+
+
+const appointments = getAppointmentsForDay(state, state.day);
+
+const schedule = appointments.map((appointment) => {
+  const interview = getInterview(state, appointment.interview);
+
+  return (
+    <Appointment
+      key={appointment.id}
+      id={appointment.id}
+      time={appointment.time}
+      interview={interview}
+    />
+  );
+});
+
+ 
 
   return (
     <main className="layout">
@@ -61,10 +81,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map((appointment) => 
-          <Appointment key={appointment.id} {...appointment} />
-           )}
-          <Appointment key="last" time="5pm" />
+        {schedule}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -2,40 +2,8 @@
 //need to iterate through comparing where that object id matches states.appointments id
 //validate for no appointments
 
-const state = {
-  days: [
-    {
-      id: 1,
-      name: "Monday",
-      appointments: [1, 2, 3]
-    },
-    {
-      id: 2,
-      name: "Tuesday",
-      appointments: [4, 5]
-    }
-  ],
-  appointments: {
-    "1": { id: 1, time: "12pm", interview: null },
-    "2": { id: 2, time: "1pm", interview: null },
-    "3": {
-      id: 3,
-      time: "2pm",
-      interview: { student: "Archie Cohen", interviewer: 2 }
-    },
-    "4": { id: 4, time: "3pm", interview: null },
-    "5": {
-      id: 5,
-      time: "4pm",
-      interview: { student: "Chad Takahashi", interviewer: 2 }
-    }
-  }
-};
-
-
-
 //Filter appointments for a given day
- export default function getAppointmentsForDay(state, day) {
+export function getAppointmentsForDay(state, day) {
 
   const filteredDays = state.days.filter(elem => elem.name === day);
 
@@ -52,4 +20,24 @@ const state = {
   }
 
   return finalAppointments;
-}
+};
+
+// Format interview data to return object with student and interviewer data
+export function getInterview(state, interview) {
+
+  if (!interview) {
+    return null
+  }
+
+  const finalInterview = {
+    ...interview,
+    interviewer: {
+      ...state.interviewers[interview.interviewer]
+    }
+  };
+
+  return finalInterview;
+};
+
+
+
